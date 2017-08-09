de9c59fdac8122c3eef477337e804aca4087a937
matsumoto <13563401+mats0693@users.noreply.github.com>
2017-08-09 10:41:46 +0900

    Update exec.sql

--- oracle_sqlplus_execBAT/exec.sql
+++ oracle_sqlplus_execBAT/exec.sql
@@ -1,11 +1,11 @@
 
 spool exec.log
 
-prompt *** ˆ—‚»‚Ì‚P ****
+prompt *** work1 ****
 select * from v$version;
 
 
-prompt *** ˆ—‚»‚Ì‚Q ****
+prompt *** work2 ****
 select to_char(sysdate,'yyyymmdd_hh24miss') log_date from dual;
 
 spool off;
